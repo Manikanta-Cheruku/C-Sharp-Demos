c8ceaaf3c8a862ff597c7bbf63faf19f690430f9
manikantacheruku <manikantacheruku0308@gmail.com>
2025-03-20 11:22:14 +0530

    Initial Commit

---
+++ DemosbyParth/Demo_CTS/ClassLibrary1/Class1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Class1
+    Public Function Calculate() As Integer
+
+        Dim a As Integer = 10
+        Dim b As Integer = 15
+        Dim c As Integer
+        c = a + b
+        Return c
+
+
+
+
+    End Function
+End Class
---
+++ DemosbyParth/Demo_CTS/Demo_CTS/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_CTS
+{
+    internal class Calculator
+    {
+        public int Calculate()
+        {
+            int a = 15, b = 15, c;
+            c = a + b;
+            return c;
+        }
+    }
+}
